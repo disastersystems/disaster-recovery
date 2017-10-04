eb0f4a0e99502e8d4c6317624292b829ebd82eb9
cubeddu <juan.cubeddu@gmail.com>
2017-10-04 11:33:59 -0500

    adjusted alerts for header and footer

--- web/modules/custom/disaster_alerts/js/alerts.js
+++ web/modules/custom/disaster_alerts/js/alerts.js
@@ -1,21 +1,40 @@
 (function ($) {
   Drupal.behaviors.alerts = {
     attach: function (context, settings) {
-      $('body').each(function() {
+      $('body').once('alerts').each(function() {
         $(document).ready(function() {
-          $('#edit-submit, #edit-submit--2').on('click', function(e) {
+          $('.alert-header > .add-phone-number-form > #edit-submit').on('click', function(e) {
             e.preventDefault();
             $.ajax({
               type: "POST",
               url: "/disaster_alerts/alert_init",
-              data: $('#add-phone-number-form').serializeArray(),
+              data: $('.alert-header > #add-phone-number-form').serializeArray(),
               error: function (xhr, ajaxOptions, thrownError) {
                 console.log('ERROR (xhr): ', xhr);
                 console.log('ERROR (ajaxOptions): ', ajaxOptions);
                 console.log('ERROR (thrownError): ', thrownError);
               },
               success: function(data) {
+                console.log('made it')
                 $(".the-return").html("<p> " + data.data + "</p>");
+              }
+            });
+
+          })
+          $('.footer-alert > form.add-phone-number-form > #edit-submit').on('click', function(e) {
+            e.preventDefault();
+            $.ajax({
+              type: "POST",
+              url: "/disaster_alerts/alert_init",
+              data: $('.footer-alert > form.add-phone-number-form').serializeArray(),
+              error: function (xhr, ajaxOptions, thrownError) {
+                console.log('ERROR (xhr): ', xhr);
+                console.log('ERROR (ajaxOptions): ', ajaxOptions);
+                console.log('ERROR (thrownError): ', thrownError);
+              },
+              success: function(data) {
+                console.log('made it here too')
+                $(".the-return-footer").html("<p> " + data.data + "</p>");
               }
             });
 
